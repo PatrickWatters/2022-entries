dcb4c59f793acc3b3949fcb38b7a1405ee9250ff
Patrick Watters <>
2023-05-25 17:45:04 -0700

    debug comments added

--- open-division/prize3-plonk-dizk/winderica/distributed/src/gpu/device/mod.rs
+++ open-division/prize3-plonk-dizk/winderica/distributed/src/gpu/device/mod.rs
@@ -10,10 +10,13 @@ pub fn build_device_list() -> CudaResult<(Vec<Device>, CudaContexts)> {
     let mut all_devices = Vec::new();
     let mut contexts = Vec::new();
     
-    println!("Number of devices: {}", 1);
-    
+    //let num_devices = rustacuda::device::Device::num_devices()?;
+    //println!("Number of devices: {}", num_devices);
+    println!("l {}", 1);
     rustacuda::init(rustacuda::CudaFlags::empty())?;
+    println!("l {}", 2);
     for device in rustacuda::device::Device::devices()? {
+    println!("l {}", 3);
         let device = device?;
         let owned_context = rustacuda::context::Context::create_and_push(
             rustacuda::context::ContextFlags::MAP_HOST
@@ -21,23 +24,27 @@ pub fn build_device_list() -> CudaResult<(Vec<Device>, CudaContexts)> {
             device,
         )?;
         rustacuda::context::ContextStack::pop()?;
-
+        println!("l {}", 4);
         let memory = device.total_memory()?;
+        println!("l {}", 5);
         let compute_units =
             device.get_attribute(rustacuda::device::DeviceAttribute::MultiprocessorCount)? as u32;
+        println!("l {}", 6);
         let compute_capability = (
             device.get_attribute(rustacuda::device::DeviceAttribute::ComputeCapabilityMajor)?
                 as u32,
             device.get_attribute(rustacuda::device::DeviceAttribute::ComputeCapabilityMinor)?
                 as u32,
+               
         );
+        println!("l {}", 7);
         let context = owned_context.get_unowned();
-
+        println!("l {}", 8);
         contexts.push(owned_context);
-
+        println!("l {}", 9);
         all_devices.push(Device { memory, compute_units, compute_capability, context });
     }
-
+    println!("l {}", 10);
     let wrapped_contexts = CudaContexts(contexts);
 
     Ok((all_devices, wrapped_contexts))
